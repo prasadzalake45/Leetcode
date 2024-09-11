32feee097a429a4b030f12f8e0129f8cc60111d5
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2024-09-11 10:59:21 +0530

    Time: 3 ms (40.66%), Space: 7.9 MB (58.53%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+
+
+        stack<char>st;
+
+        for(char ch:s){
+            if(ch=='(' || ch=='[' || ch=='{'){
+                st.push(ch);
+            }
+            else{
+                if(st.empty() || (ch==')' && st.top()!='(' )||(ch==']' && st.top()!='[') || (ch=='}' && st.top()!='{') ){
+                    return false;
+                }
+                else{
+                    st.pop();
+                }
+            }
+        }
+
+        return st.empty();
+        
+    }
+};
