3f0f700c464f0e906f30781919e2052db3fe7f12
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2024-09-17 10:44:31 +0530

    Time: 2 ms (51.8%), Space: 9.3 MB (16.29%) - LeetHub

--- 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.cpp
+++ 0920-uncommon-words-from-two-sentences/0920-uncommon-words-from-two-sentences.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
     vector<string> uncommonFromSentences(string s1, string s2) {
-        
+        unordered_map<string,int>mp1;
+        unordered_map<string,int>mp2;
 
-        unordered_map<string,int>m1;
-        
+         stringstream ss(s1);
+         stringstream ss2(s2);
+         string word;
+         while(ss>>word){
+            mp1[word]++;
+         }
+         string word2;
+         while(ss2>>word2){
+            mp2[word2]++;
+         }
 
-        stringstream ss1(s1),ss2(s2);
-        string word;
+         vector<string>vec;
 
-        while(ss1>>word){
-            m1[word]++;
-        }
-        while(ss2>>word){
-            m1[word]++;
-        }
-        vector<string>vec;
-
-
-        for(auto it:m1){
-            if(it.second==1){
+         for(auto it:mp1){
+            if(it.second==1 && mp2[it.first]==0){
                 vec.push_back(it.first);
             }
-        }
+         }
+         for(auto it:mp2){
+            if(it.second==1 && mp1[it.first]==0){
+                vec.push_back(it.first);
+            }
+         }
 
-        return vec;
+         return vec;
     }
 };
