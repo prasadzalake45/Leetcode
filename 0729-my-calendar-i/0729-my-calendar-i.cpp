ee177b5ee4ab805750952648c49c133665f4c52f
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2024-09-27 18:42:10 +0530

    Time: 212 ms (14.45%), Space: 47.7 MB (10.92%) - LeetHub

---
+++ 0729-my-calendar-i/0729-my-calendar-i.cpp
@@ -0,0 +1,33 @@
+class MyCalendar {
+
+    map<int,int>mp;
+public:
+    MyCalendar() {
+        
+    }
+    
+    bool book(int start, int end) {
+
+        mp[start]++;
+        mp[end]--;
+
+        int sum=0;
+
+        for(auto it:mp){
+            sum+=it.second;
+            if(sum>=2){
+                mp[start]--;
+                mp[end]++;
+                return false;
+            }
+        }
+        return true;
+        
+    }
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
