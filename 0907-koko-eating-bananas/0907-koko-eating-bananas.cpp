bcd1edb9bdb845f2de0b2f6ea0de05c9db76be4d
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2024-09-28 17:27:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+
+    
+    int helper(vector<int>&piles,long r){
+        
+        long long  totalhr=0;
+        for(int i=0;i<piles.size();i++){
+            totalhr+=ceil((double)piles[i]/(double)r);
+        }
+        
+
+        return totalhr;
+
+
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+
+        int low=1;
+        int high=*max_element(piles.begin(),piles.end());
+        int cur=INT_MAX;
+
+        while(low<=high){
+            long mid=(low+high)/2;
+
+            long long  ans=helper(piles,mid);
+
+            if(ans<=h){
+                cur=mid;
+                high=mid-1;
+
+                
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return cur;
+
+        
+
+
+
+        
+    }
+};
