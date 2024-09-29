8ccb4953e5790d7e9db485843dd0df7015ca8d52
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2024-09-29 22:38:05 +0530

    Time: 10 ms (49.04%), Space: 21.8 MB (71.32%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -17,15 +17,19 @@ public:
             return 0;
         }
         int lh=check(root->left);
-
         if(lh==-1){
             return -1;
         }
+
+
+       
         int rh=check(root->right);
 
         if(rh==-1){
             return -1;
         }
+
+      
 
         if(abs(lh-rh)>1){
             return -1;
