e6dbf49036c3fb1c54143bbba38ec137c0dae84e
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2024-09-30 10:18:59 +0530

    Time: 0 ms (100%), Space: 8.4 MB (85.16%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -2,31 +2,31 @@ class Solution {
 public:
     bool isIsomorphic(string s, string t) {
 
-        if(s.size()!=t.size()){
+        int n1=s.size();
+        int n2=t.size();
+
+        if(n1!=n2){
             return false;
         }
 
-       vector<int>smap(256,0);
-       vector<int>tmap(256,0);
+        vector<int>vec1(128,0);
+        vector<int>vec2(128,0);
 
+        for(int i=0;i<n1;i++){
 
-       for(int i=0;i<s.size();i++){
-        if(smap[s[i]]==0){
-            smap[s[i]]=i+1;
+            if(vec1[s[i]]==0){
+                vec1[s[i]]=i+1;
+            }            
+            if(vec2[t[i]]==0){
+               vec2[t[i]]=i+1;
+            }
+            if(vec1[s[i]]!=vec2[t[i]]){
+                return false;
+            }
+
         }
-        if(tmap[t[i]]==0){
-            tmap[t[i]]=i+1;
-        }
-        if(smap[s[i]]!=tmap[t[i]]){
-            return false;
-        }
-       }
 
-
-       return true;
-
-
-      
+        return true;
         
     }
 };
