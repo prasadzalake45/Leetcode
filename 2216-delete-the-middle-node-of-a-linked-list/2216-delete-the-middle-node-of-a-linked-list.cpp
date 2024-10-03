3a015a694ffaa851f73930adadcfc2ec3529676b
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2024-10-04 01:11:13 +0530

    Time: 620 ms (27.31%), Space: 304.4 MB (6.75%) - LeetHub

--- 2216-delete-the-middle-node-of-a-linked-list/2216-delete-the-middle-node-of-a-linked-list.cpp
+++ 2216-delete-the-middle-node-of-a-linked-list/2216-delete-the-middle-node-of-a-linked-list.cpp
@@ -1,35 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    // Helper function to find and delete the middle node
-    int deleteMiddleHelper(ListNode* &current, int length, int count) {
-        // Base case: If we reach the end of the list, return 0
-        if (current == nullptr) {
-            return 0;
-        }
 
-        // Recurse for the next node
-        int index = deleteMiddleHelper(current->next, length, count + 1);
+   int removeMiddle(ListNode* head,int length,int cnt){
 
-        // If the current node is the one before the middle node
-        if (count == length / 2 - 1) {
-            current->next = current->next->next; // Skip the middle node
-        }
-
-        return index + 1;
+    if(head==NULL){
+        return 0;
     }
 
+    int index=removeMiddle(head->next,length,cnt+1);
+
+    if(cnt==(length/2)-1){
+        head->next=head->next->next;
+    }
+
+    return index+1;
+
+    
+   }
     ListNode* deleteMiddle(ListNode* head) {
-        // If the list is empty or has only one node, return NULL
-        if (head == nullptr || head->next == nullptr) {
-            return nullptr;
+
+        if(head==NULL || head->next==NULL){
+            return NULL;
         }
 
-        // Find the length of the list by recursively traversing it
-        int length = deleteMiddleHelper(head, 0, 0);
+        int length=removeMiddle(head,0,0);
 
-        // Delete the middle node in the second pass
-        deleteMiddleHelper(head, length, 0);
+        removeMiddle(head,length,0);
 
         return head;
+        
     }
 };
