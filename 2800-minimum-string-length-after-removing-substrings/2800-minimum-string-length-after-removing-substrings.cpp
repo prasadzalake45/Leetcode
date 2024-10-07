b5ea0668a2158abf6b41d869c53c4aaa1dbd7280
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2024-10-07 19:03:26 +0530

    Time: 4 ms (76.08%), Space: 11.6 MB (31.24%) - LeetHub

---
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minLength(string s) {
+        stack<char> st;
+
+        for(char ch : s) {
+            if(!st.empty()) {
+                
+                if((ch == 'B' && st.top() == 'A') || (ch == 'D' && st.top() == 'C')) {
+                    st.pop(); 
+                } else {
+                    st.push(ch); 
+                }
+            } else {
+                st.push(ch); 
+            }
+        }
+
+        return st.size(); 
+    }
+};
