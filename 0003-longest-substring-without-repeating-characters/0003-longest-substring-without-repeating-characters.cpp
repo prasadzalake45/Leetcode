e212bdc1de3d1a3ee6639f7aa2a07ea6a8b055d2
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2024-10-08 18:59:46 +0530

    Time: 25 ms (24.76%), Space: 14.7 MB (17.88%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,42 +1,37 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        
+
+        unordered_set<char>seti;
 
         int i=0;
-        int n=s.size();
         int j=0;
-        unordered_map<char,int>mp;
-        if(n==0){
-            return 0;
-        }
-        int maxi=INT_MIN;
+        int n=s.size();
+        int maxi=0;
+        int len=0;
 
         while(j<n){
+            if(seti.find(s[j])==seti.end()){
 
-            mp[s[j]]++;
-
-           
-            if(mp.size()==j-i+1){
-                maxi=max(maxi,j-i+1);
-                j++;
+                seti.insert(s[j]);
+                int len=seti.size();
                 
-
-            }
-            else if(mp.size()<j-i+1){
-
-                while(mp.size()<j-i+1){
-                    mp[s[i]]--;
-                    if(mp[s[i]]==0){
-                        mp.erase(s[i]);
-                    }
-                    i++;
-                }
+                maxi=max(maxi,len);
                 j++;
+            }
+
+            else{
+              
+                seti.erase(s[i]);
+                i++;
+              
+
+
             }
         }
 
         return maxi;
+
         
     }
 };
