6770be314f86d58708806a8fdd086870f917b952
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2024-10-14 11:35:31 +0530

    Time: 883 ms (5.24%), Space: 76.2 MB (77.56%) - LeetHub

---
+++ 2616-maximal-score-after-applying-k-operations/2616-maximal-score-after-applying-k-operations.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    long long maxKelements(vector<int>& nums, int k) {
+
+        int n=nums.size();
+
+
+        priority_queue<int>pq(nums.begin(),nums.end());
+        
+       long long score=0;
+
+       while(!pq.empty() && k>0){
+
+        double ans=pq.top();
+        cout<<ans<<endl;
+        pq.pop();
+
+        score+=ans;
+        double res=ans/3.0;
+
+
+
+        k--;
+
+        pq.push(ceil(res));
+
+       }
+
+       return score;
+        
+    }
+};
