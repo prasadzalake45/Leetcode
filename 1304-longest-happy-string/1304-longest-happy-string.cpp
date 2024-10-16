8c28dfc584158af8799811d491cf9b93cac3c8d6
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2024-10-16 12:18:58 +0530

    Time: 3 ms (43.32%), Space: 7.8 MB (64.09%) - LeetHub

--- 1304-longest-happy-string/1304-longest-happy-string.cpp
+++ 1304-longest-happy-string/1304-longest-happy-string.cpp
@@ -3,24 +3,22 @@ public:
     string longestDiverseString(int a, int b, int c) {
 
         priority_queue<pair<int,char>>pq;
-        if(a>0)pq.push({a,'a'});
-         if(b>0)pq.push({b,'b'});
-        if(c>0)pq.push({c,'c'});
 
-
-        string ans="";
-
+        if(a>0) pq.push({a,'a'});
+        if(b>0) pq.push({b,'b'});
+        if(c>0) pq.push({c,'c'});
+       
+       string ans="";
 
         while(!pq.empty()){
-
             auto it=pq.top();
             pq.pop();
 
             if(ans.size()>=2 && ans.back()==it.second && ans[ans.size()-2]==it.second){
-                if (pq.empty()) {
+
+                if(pq.empty()){
                     break;
                 }
-                
                 auto it2=pq.top();
                 pq.pop();
 
@@ -32,21 +30,15 @@ public:
                 }
 
                 pq.push({it.first,it.second});
+            } 
 
-
-
-            }
             else{
-
                 ans+=it.second;
                 it.first--;
 
                 if(it.first>0){
                     pq.push({it.first,it.second});
                 }
-
-
-                
             }
         }
 
