d25232349c077b2fc1b4a82f75303e131872bdf2
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2024-10-20 10:31:07 +0530

    Time: 68 ms (50%), Space: 165.5 MB (50%) - LeetHub

--- 3502-count-substrings-with-k-frequency-characters-i/3502-count-substrings-with-k-frequency-characters-i.cpp
+++ 3502-count-substrings-with-k-frequency-characters-i/3502-count-substrings-with-k-frequency-characters-i.cpp
@@ -1,30 +1,46 @@
 class Solution {
 public:
-    int numberOfSubstrings(string s, int k) {
-        int n = s.length();
-    int result = 0;
 
-    for (int left = 0; left < n; ++left) {
-        unordered_map<char, int> freqMap;
-        
-        for (int right = left; right < n; ++right) {
-            char currentChar = s[right];
-            freqMap[currentChar]++;
-            
-            bool validSubstring = false;
-            for (auto& [ch, freq] : freqMap) {
-                if (freq >= k) {
-                    validSubstring = true;
-                    break;
-                }
-            }
-            
-            if (validSubstring) {
-                result++;
-            }
+   bool ischeck(unordered_map<char,int>&mp,string s,int k){
+     
+
+     for(auto it:mp){
+        if(it.second>=k){
+            return true;
         }
-    }
-    return result;
-        
+     }
+
+     return false;
+   }
+    int numberOfSubstrings(string s, int k) {
+
+        unordered_map<char,int>mp;
+        int i=0;
+        int j=0;
+        int n=s.size();
+
+        int cnt=0;
+
+        while(j<n){
+            mp[s[j]]++;
+
+            while(ischeck(mp,s,k)==true){
+
+                cnt+=n-j;
+
+                mp[s[i]]--;
+
+                if(mp[s[i]]==0){
+                    mp.erase(s[i]);
+                }
+                i++;
+
+
+
+            }
+
+            j++;
+        }
+        return cnt;
     }
 };
