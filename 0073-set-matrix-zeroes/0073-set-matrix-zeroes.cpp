33e7ca95be394a1b8faddd2bd36477db6b410077
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2024-11-22 16:21:12 +0530

    Time: 0 ms (100%), Space: 17 MB (57.39%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,60 +1,33 @@
 class Solution {
-public:  
-   
-    void markrow(int row,int col,vector<vector<int>>& matrix){
-
-        for(int col=0;col<matrix[0].size();col++){
-            if(matrix[row][col]!=0){
-                matrix[row][col]='p';
-            }
-        }
-
-    }
-
-    void markcol(int row,int col,vector<vector<int>>& matrix){
-
-        for(int row=0;row<matrix.size();row++){
-            if(matrix[row][col]!=0){
-                matrix[row][col]='p';
-            }
-        }
-        
-    }
-
-
-    
+public:
     void setZeroes(vector<vector<int>>& matrix) {
+
+
         int n=matrix.size();
         int m=matrix[0].size();
-        
+
+        vector<int> row(n, 0);
+        vector<int> col(m, 0);
+
+
+          
+ 
         for(int i=0;i<n;i++){
-           
             for(int j=0;j<m;j++){
                 if(matrix[i][j]==0){
-                    markrow(i,j,matrix);
-                    markcol(i,j,matrix);
-                   
-                    
-                    
+                    row[i]=1;
+                    col[j]=1;
                 }
             }
         }
 
-
-         for(int i=0;i<n;i++){
-           
+        for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(matrix[i][j]=='p'){
+                if(row[i]==1 || col[j]==1){
                     matrix[i][j]=0;
-                    
-                    
                 }
             }
         }
-
-       
-
         
-         
     }
 };
