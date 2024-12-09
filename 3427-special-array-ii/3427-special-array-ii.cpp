c4ea6d0fff77dbf2d1cc982393d5690107f3acc1
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2024-12-09 21:32:31 +0530

    Time: 4 ms (87.5%), Space: 126.2 MB (73.63%) - LeetHub

--- 3427-special-array-ii/3427-special-array-ii.cpp
+++ 3427-special-array-ii/3427-special-array-ii.cpp
@@ -1,46 +1,57 @@
 class Solution {
-public:
+public:  
+
+  bool bSearch(vector<int>&index,int start,int end){
+    int l=0;
+    int h=index.size()-1;
+
+    while(l<=h){
+        int mid=(l+h)/2;
+
+        if(index[mid]<start){
+            l=mid+1;
+        }
+        else if(index[mid]>end){
+            h=mid-1;
+        }
+        else{
+            return false;
+        }
+    }
+
+    return true;
+
+  }
     vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
+        int n=nums.size();
+        vector<int>index;
+        vector<bool>ans;
 
-   int n=nums.size();
+        for(int i=1;i<n;i++){
+            if(nums[i]%2==nums[i-1]%2){
+                index.push_back(i);
+            }
+        }
 
-   vector<int>ans(n,0);
-
-   int i=0;
-   int j=0;
-
-   while(i<n){
-
-    if(j<i){
-        j=i;
-    }
-
-    while(j+1<n && nums[j]%2!=nums[j+1]%2 ){
-        j++;
-
-    }
-
-    ans[i]=j;
-    i++;
-    
-
-   }
-    vector<bool>res;
-   for(int i=0;i<queries.size();i++){
-    int start=queries[i][0];
-    int end=queries[i][1];
-
-    if(ans[start]>=end){
-        res.push_back(true);
-    }
-    else{
-        res.push_back(false);
-    }
+        for(int i=0;i<queries.size();i++){
+            int start=queries[i][0];
+            int end=queries[i][1];
 
 
-   }
 
-   return res;
-        
-    }
+        bool violate=bSearch(index,start+1,end);
+        if(violate==false){
+            ans.push_back(false);
+
+        }
+        else{
+            ans.push_back(true);
+        }
+
+
+
+        }     
+
+        return ans;
+     }
 };
