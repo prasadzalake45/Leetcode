3dcd3bacc9c26a096f5104587bf499fa40799d08
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2025-01-17 10:14:54 +0530

    Time: 7 ms (29.44%), Space: 263.9 MB (74.77%) - LeetHub

--- 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
@@ -1,33 +1,13 @@
 class Solution {
 public:
     bool doesValidArrayExist(vector<int>& derived) {
-        int n=derived.size();
+        int XOR=0;
 
-        vector<int>ori(n);
 
-        ori[0]=0;
-
-        for(int i=1;i<n;i++){
-            ori[i]=derived[i-1]^ori[i-1];
-
+        for(int i=0;i<derived.size();i++){
+            XOR=XOR^derived[i];
         }
 
-        if(ori[n-1]^ori[0]==derived[n-1]){
-            return true;
-        }
-        
-
-        ori[0]=1;
-         for(int i=1;i<n;i++){
-            ori[i]=derived[i-1]^ori[i-1];
-
-        }
-
-        if(ori[n-1]^ori[0]==derived[n-1]){
-            return true;
-        }
-
-
-        return false;
+        return XOR==0;
     }
 };
