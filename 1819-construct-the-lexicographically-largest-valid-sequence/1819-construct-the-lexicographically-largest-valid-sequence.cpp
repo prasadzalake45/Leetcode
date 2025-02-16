dcf3c9d4947e15300f89d7d6d546b11514c092b6
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2025-02-16 16:17:12 +0530

    Time: 0 ms (100%), Space: 8.7 MB (36.8%) - LeetHub

---
+++ 1819-construct-the-lexicographically-largest-valid-sequence/1819-construct-the-lexicographically-largest-valid-sequence.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+
+    bool solve(int idx,int n,vector<int>&result,vector<bool>&used){
+        if(idx>=result.size()){
+            return true;
+        }
+
+        if(result[idx]!=-1){
+            return  solve(idx+1,n,result,used);
+        }
+
+
+        for(int num=n;num>=1;num--){
+            if(used[num]){
+                continue;
+            }
+
+            //try
+
+            used[num]=true;
+            result[idx]=num;
+          
+            //Explore
+
+            if(num==1){
+                if(solve(idx+1,n,result,used)==true){
+                    return true;
+                }
+            }
+            else{
+                int j=result[idx]+idx;
+                if(j<result.size() && result[j]==-1){
+                    result[j]=num;
+                    if(solve(idx+1,n,result,used)==true){
+                        return true;
+                    }
+                    result[j]=-1;
+                }
+            }
+
+
+            //Undo
+  
+
+            used[num]=false;
+            result[idx]=-1;
+
+
+
+
+
+        }
+
+        return false;
+    }
+    vector<int> constructDistancedSequence(int n) {
+        vector<int>result(2*n-1,-1);
+
+        vector<bool>used(n+1,false);
+
+        solve(0,n,result,used);
+
+        return result;
+    }
+};
