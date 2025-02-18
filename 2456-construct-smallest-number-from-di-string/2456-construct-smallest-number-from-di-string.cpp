0e974a30ca9fc0ed4865440ca51158e9b21727f1
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2025-02-18 23:53:30 +0530

    Time: 23 ms (12.78%), Space: 7.5 MB (99.59%) - LeetHub

---
+++ 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+
+   bool matchedPattern(string& num,string& pattern){
+    for(int i=0;i<pattern.size();i++){
+        if((pattern[i]=='I' && num[i]>num[i+1]) ||(pattern[i]=='D' && num[i]<num[i+1])){
+          return false;
+        }
+    }
+
+    return true;
+   }
+    string smallestNumber(string pattern) {
+
+        int n=pattern.size();
+        string num="";
+
+        for(int i=1;i<=n+1;i++){
+            num.push_back(i+'0');
+
+
+        }
+
+        while(!matchedPattern(num,pattern)){
+            next_permutation(begin(num),end(num));
+        }
+
+
+        return num;
+        
+    }
+};
