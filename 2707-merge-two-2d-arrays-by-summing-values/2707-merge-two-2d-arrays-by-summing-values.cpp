29301b76f4887c0d2223182dd2bb126df7bd56ee
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2025-03-03 00:24:06 +0530

    Time: 0 ms (100%), Space: 14.7 MB (82.78%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.cpp
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.cpp
@@ -2,31 +2,61 @@ class Solution {
 public:
     vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
 
-        unordered_map<int,int>mp;
-       
-         
-        for(auto it:nums1){
-            mp[it[0]]+=it[1];
-        }
+        int n1=nums1.size();
+        int n2=nums2.size();
 
 
-        for(auto it:nums2){
-            mp[it[0]]+=it[1];
-        }
-
+        int i=0;
+        int j=0;
 
         vector<vector<int>>ans;
 
-        for(auto it:mp){
+        while(i<n1 && j<n2){
+            if(nums1[i][0]==nums2[j][0]){
+                // ans[i][0]=nums1[i][0];
+                // ans[i][1]=nums1[i][1]+nums2[j][1];
+               
+                ans.push_back({nums1[i][0],nums1[i][1]+nums2[j][1]});
+                 i++;
+                j++;
+             
 
-           ans.push_back({it.first,it.second});
+            }
+
+            else if(nums1[i][0]<nums2[j][0]){
+                // ans[i][0]=nums1[i][0];
+                // ans[i][1]=nums1[1][1];
+                ans.push_back({nums1[i][0],nums1[i][1]});
+                i++;
+            }
+            else{
+
+                // ans[i][0]=nums2[j][0];
+                // ans[i][1]=nums2[j][1];
+                ans.push_back({nums2[j][0],nums2[j][1]});
+                j++;
+
+            }
 
 
-       }
+
+        }
+
+        while(i<n1){
+             ans.push_back({nums1[i][0],nums1[i][1]});
+                i++;
 
 
-       sort(ans.begin(),ans.end());
-       return ans;
+        }
+        while(j<n2){
+              ans.push_back({nums2[j][0],nums2[j][1]});
+                j++;
+
+
+
+        }
+
+        return ans;
         
     }
 };
