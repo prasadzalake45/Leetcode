4ba933616ab004c61f05fc11b566bdf4a2632f64
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2025-03-11 23:47:35 +0530

    Time: 59 ms (5.72%), Space: 16.9 MB (8.98%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -4,27 +4,27 @@ public:
 
         int n=s.size();
 
+        unordered_map<char,int>mp;
 
         int i=0;
         int j=0;
-  
-
-        unordered_map<char,int>mp;
-
         int cnt=0;
+
         while(j<n){
 
             mp[s[j]]++;
 
-
             while(mp['a']>0 && mp['b']>0 && mp['c']>0){
                 cnt+=(n-j);
                 mp[s[i]]--;
+
+                if(mp[s[i]]==0){
+                    mp.erase(s[i]);
+                }
                 i++;
+
             }
             j++;
-
-
         }
 
         return cnt;
