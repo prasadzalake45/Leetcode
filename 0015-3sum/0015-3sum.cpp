d27479b31e564aa57ba12d76a2f798c22274c3da
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2025-03-30 19:26:35 +0530

    Time: 1280 ms (8.11%), Space: 295.7 MB (5.67%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,49 +1,40 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        int n=nums.size();
 
-        sort(nums.begin(), nums.end());
+        sort(nums.begin(),nums.end());
 
-        int n = nums.size();
-        vector<int> tri;
-        vector<vector<int>> ans;
+        set<vector<int>>ans;
 
-        for (int i = 0; i < n; i++) {
+        for(int i=0;i<n-2;i++){
+            int start=i+1;
+            int end=n-1;
 
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-
-            int j = i + 1;
-            int k = n - 1;
-
-            while (j < k) {
-
-                if (nums[i] + nums[j] + nums[k] == 0) {
-                    tri = {nums[i], nums[j], nums[k]};
-                    ans.push_back(tri);
-
-                    while (j < k && nums[j] == nums[j + 1]) {
-                        j++;
-                    }
-                    while (j < k && nums[k] == nums[k - 1]) {
-                        k--;
-                    }
-
-                    j++;
-                    k--;
-
+            while(start<end){
+                if(nums[i]+nums[start]+nums[end]==0){
+                    ans.insert({nums[i],nums[start],nums[end]});
+                    start++;
+                    end--;
+                    
                 }
-
-                else if (nums[i] + nums[j] + nums[k] > 0) {
-                    k--;
-                } else {
-                    j++;
+                else if(nums[i]+nums[start]+nums[end]>0){
+                    end--;
+                }
+                else{
+                    start++;
                 }
             }
+
+
         }
 
+        vector<vector<int>>sll;
+        for(auto it:ans){
+            sll.push_back(it);
+        }
+
+        return sll;
         
-        return ans;
     }
 };
