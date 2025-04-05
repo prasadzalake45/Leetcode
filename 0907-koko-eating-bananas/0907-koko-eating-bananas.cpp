b17beb89d4386280a4ffe14c0c6ec9d3513ee3f9
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2025-04-05 23:47:37 +0530

    Time: 19 ms (13.83%), Space: 22.8 MB (74.72%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -1,43 +1,37 @@
 class Solution {
 public:
 
-    bool isPossible(vector<int>&piles,int mid,int h){
-        long long total=0;
-
-
+    long long  helper(vector<int>&piles,int mid){
+        long long sum=0;
         for(int i=0;i<piles.size();i++){
-            total+=ceil((double)piles[i]/mid);
+            sum+=ceil(double(piles[i])/mid);
+           
+            
         }
+        return sum;
 
-        if(total>h){
-            return false;
-        }
-
-        return true;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
 
-        int low=1;
+        int n=piles.size();
 
-        long long high=*max_element(piles.begin(),piles.end());
+        int mini=1;
+        int maxi=*max_element(piles.begin(),piles.end());
+        long ans=maxi;
+        while(mini<=maxi){
+            int mid=(mini+maxi)/2;
+            if(helper(piles,mid)<=h){
+                ans=mid;
+                maxi=mid-1;
 
-        long long result=high;
-
-        while(low<=high){
-
-            long long mid=(low+high)/2;
-
-            if(isPossible(piles,mid,h)){
-                result=mid;
-                high=mid-1;
             }
             else{
-                low=mid+1;
+                mini=mid+1;
             }
-
         }
+        return ans;
 
-        return result;
+
         
     }
 };
