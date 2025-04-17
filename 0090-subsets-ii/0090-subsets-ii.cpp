374766e355414d22f69aedb4e9a35d88240e2e11
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2025-04-17 19:23:38 +0530

    Time: 0 ms (100%), Space: 10.5 MB (54.8%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
-    void findcomb(int index,vector<int>&arr,vector<int>ds,vector<vector<int>>&result){
-        result.push_back(ds);
 
-        for(int i=index;i<arr.size();i++){
-            if(i!=index && arr[i]==arr[i-1]) continue;
+   void helper(int index,vector<int>&nums,vector<int>&ds,vector<vector<int>>&ans){
 
-            ds.push_back(arr[i]);
-            findcomb(i+1,arr,ds,result);
-            ds.pop_back();
+    ans.push_back(ds);
 
+    for(int i=index;i<nums.size();i++){
+        if(i!=index && nums[i]==nums[i-1]){
+            continue;
         }
+        ds.push_back(nums[i]);
+        helper(i+1,nums,ds,ans);
+        ds.pop_back();
     }
+   
+   }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<vector<int>>result;
-        vector<int>ds;
-        sort(nums.begin(),nums.end());
-        findcomb(0,nums,ds,result);
-        return result;
 
+        vector<int>ds;
+        vector<vector<int>>ans;
+        int index=0;
+        sort(nums.begin(),nums.end());
+
+        helper(index,nums,ds,ans);
+
+        return ans;
         
     }
 };
