1be8782f799376194ddde33a178b27f8bc5ab87b
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2025-04-26 00:32:27 +0530

    Time: 19 ms (31.37%), Space: 14.2 MB (31.25%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -3,40 +3,33 @@ public:
     int lengthOfLongestSubstring(string s) {
 
         int n=s.size();
-
         if(n==0){
             return 0;
         }
 
         int i=0;
         int j=0;
-        unordered_map<int,int>mp;
+        unordered_set<int>mp;
         int maxi=1;
 
         while(j<n){
 
-            if(mp.find(s[j])!=mp.end()){
+             while(mp.find(s[j])!=mp.end()){
+                mp.erase(s[i]);
+                i++;
+                
 
-               
-                i=max(i,mp[s[j]]+1);
-            
-
-
-
-              
             }
-            mp[s[j]]=j;
             maxi=max(maxi,j-i+1);
+            mp.insert(s[j]);
             j++;
 
 
+
+
+
         }
-
-        // maxi=max(maxi,(int)mp.size());
-
-        
-
-        return  maxi;
+        return maxi;
         
     }
 };
