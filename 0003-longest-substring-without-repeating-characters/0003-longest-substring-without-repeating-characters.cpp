2565448382a4a1e9972a70354f6c877a3abe1f4e
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2025-04-26 00:18:08 +0530

    Time: 5 ms (73.41%), Space: 12 MB (49.1%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -3,46 +3,40 @@ public:
     int lengthOfLongestSubstring(string s) {
 
         int n=s.size();
+
         if(n==0){
             return 0;
         }
 
-        
+        int i=0;
+        int j=0;
+        unordered_map<int,int>mp;
         int maxi=1;
-        // bool duplicate=false;
 
-        for(int i=0;i<n;i++){
-           int arr[255]={0};
+        while(j<n){
+
+            if(mp.find(s[j])!=mp.end()){
+
+               
+                i=max(i,mp[s[j]]+1);
             
-            for(int j=i;j<n;j++){
-                if(arr[s[j]]==1){
-                //    duplicate=true; 
-                maxi=max(maxi,j-i);
-                break;
 
-                }
-                else{
-                    arr[s[j]]=1;
-                            maxi=max(maxi,j-i+1);
-                    
 
-                }
+
               
-             
-
-            
             }
+            mp[s[j]]=j;
+            maxi=max(maxi,j-i+1);
+            j++;
+
 
         }
 
-        // if(duplicate==false){
-        //     maxi=max(maxi,n);
-
-        // }
+        // maxi=max(maxi,(int)mp.size());
 
         
 
-        return maxi;
+        return  maxi;
         
     }
 };
