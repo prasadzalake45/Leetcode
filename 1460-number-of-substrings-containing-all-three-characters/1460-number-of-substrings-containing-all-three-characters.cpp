421da6bd4ed393d785da4e70eeacb54d3a1b77d1
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2025-04-27 12:30:06 +0530

    Time: 50 ms (5.72%), Space: 16.8 MB (9.4%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -3,15 +3,15 @@ public:
     int numberOfSubstrings(string s) {
 
         int n=s.size();
-
-        unordered_map<char,int>mp;
-
+        unordered_map<int,int>mp;
+        
         int i=0;
         int j=0;
+
         int cnt=0;
 
         while(j<n){
-
+            
             mp[s[j]]++;
 
             while(mp['a']>0 && mp['b']>0 && mp['c']>0){
@@ -22,12 +22,12 @@ public:
                     mp.erase(s[i]);
                 }
                 i++;
-
             }
             j++;
         }
 
         return cnt;
+
         
     }
 };
