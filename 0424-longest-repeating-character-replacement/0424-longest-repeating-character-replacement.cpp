85a23a51634a47f005bb29f8837f36d1e54e73d3
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2025-05-02 01:42:47 +0530

    Time: 6 ms (54.83%), Space: 11.2 MB (8.71%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -6,37 +6,28 @@ public:
 
         int i=0;
         int j=0;
-        unordered_map<int,int>mp;
-        int maximum=0;
-        int maxCnt=0;
+        int cnt=0;
+        int maxi=0;
+        unordered_map<char,int>mp;
 
         while(j<n){
 
             mp[s[j]]++;
-           
-            maxCnt=max(maxCnt,mp[s[j]]);
 
-            int req=(j-i+1)-maxCnt;
+            cnt=max(cnt,mp[s[j]]);
 
-            if(req<=k){
-                maximum=max(maximum,j-i+1);
+            if((j-i+1)-cnt>k){
+                mp[s[i]]--;
+                if(mp[s[i]]==0){
+                    mp.erase(s[i]);
+                }
+                i++;
             }
 
-             
-        if(req>k){
-            mp[s[i]]--;
-            if(mp[s[i]]==0){
-                mp.erase(s[i]);
-            }
-            i++;
-        }
-        
-        j++;
-
-
+            maxi=max(maxi,j-i+1);
+            j++;
 
         }
-        return maximum;
-        
+        return maxi;
     }
 };
