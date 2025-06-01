7b117d3848830c9c5f803aebb94712396974ad26
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2025-06-01 10:30:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,58 +1,84 @@
 class Solution {
 public:
-    int helper(vector<vector<int>>& grid, queue<pair<int, int>>& q,
-               int& fresh) {
-        int cnt = 0;
-        while (!q.empty()) {
-            int size = q.size();
-            bool happen = false;
 
-            for (int i = 0; i < size; i++) {
+   int bfs(int row,int col,vector<vector<int>>&grid,vector<vector<int>>&visited){
 
-                auto [x, y] = q.front();
-                q.pop();
+    queue<pair<int,int>>q;
+    q.push({row,col});
+    visited[row][col]=2;
+    int cnt=0;
+    
 
-                int dx[] = {0, 1, 0, -1};
-                int dy[] = {1, 0, -1, 0};
+    while(!q.empty()){
 
-                for (int i = 0; i < 4; i++) {
-                    int newX = x + dx[i];
-                    int newY = y + dy[i];
+      int size=q.size();
+       bool taken=false;
 
-                    if (newX >= 0 && newX < grid.size() && newY >= 0 &&
-                        newY < grid[0].size() && grid[newX][newY] == 1) {
-                        grid[newX][newY] = 2;
-                        q.push({newX, newY});
-                        fresh--;
-                        happen = true;
-                    }
-                }
-            }
 
-            if (happen) {
-                cnt++;
-            }
+      for(int i=0;i<size;i++){
+
+      
+      auto [x,y]=q.front();
+      q.pop();
+      
+    
+      int dx[]={0,1,0,-1};
+      int dy[]={1,0,-1,0};
+    
+
+      for(int i=0;i<4;i++){
+       
+        int newX=x+dx[i];
+        int newY=y+dy[i];
+
+        if(newX>=0 && newX<grid.size() && newY>=0 && newY<grid[0].size() && grid[newX][newY]==1 && visited[newX][newY]!=2){
+          
+          visited[newX][newY]=2;
+          q.push({newX,newY});
+          taken=true;
+          
         }
+      }
+      
 
-        return fresh == 0 ? cnt : -1;
+      }
+
+    if(taken){
+      cnt++;
     }
+
+
+
+    }
+    return cnt;
+
+
+   }
     int orangesRotting(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
+      int m=grid.size();
+      int n=grid[0].size();
+      vector<vector<int>>visited=grid;
+      int ans=0;
 
-        queue<pair<int, int>> q;
-        int fresh = 0;
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 2) {
-                    q.push({i, j});
-                } else if (grid[i][j] == 1) {
-                    fresh++;
-                }
-            }
+      for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+          if(grid[i][j]==2){
+            ans=bfs(i,j,grid,visited);
+          }
         }
+      }
 
-        return helper(grid, q, fresh);
+      for(int i=0;i<visited.size();i++){
+        for(int j=0;j<visited[0].size();j++){
+          if(visited[i][j]==1){
+            return -1;
+          }
+          // cout<<visited[i][j]<<" ";
+        }
+        // cout<<endl;
+      }
+
+      return ans;
+       
     }
 };
