e3032b2b3608087efca2dae5a640e8b11676386f
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2025-06-01 19:43:01 +0530

    Time: 0 ms (100%), Space: 17.9 MB (98.99%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,47 +1,28 @@
 class Solution {
 public:
-   void bfs(vector<vector<int>>&image,int sr,int sc,int color){
-      queue<pair<int,int>>q;
-      q.push({sr,sc});
+
+    void dfs(vector<vector<int>>&image,int sr,int sc,int color,int oldColor){
+
+      if(sr<0 || sr>=image.size() || sc<0 || sc>=image[0].size() || image[sr][sc]!=oldColor){
+        return;
+
+      }
+
+      image[sr][sc]=color;
+
+      dfs(image,sr+1,sc,color,oldColor);
+      dfs(image,sr-1,sc,color,oldColor);
+      dfs(image,sr,sc+1,color,oldColor);
+      dfs(image,sr,sc-1,color,oldColor);
+
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
       int oldColor=image[sr][sc];
 
       if(oldColor==color){
-        return;
+        return image;
       }
-      image[sr][sc]=color;
-     
-      
-
-
-      while(!q.empty()){
-        auto[x,y]=q.front();
-        q.pop();
-
-        int dx[]={0,1,0,-1};
-        int dy[]={1,0,-1,0};
-       
-
-        for(int i=0;i<4;i++){
-          int newX=x+dx[i];
-          int newY=y+dy[i];
-
-          if(newX>=0 && newX<image.size() && newY>=0 && newY<image[0].size() && image[newX][newY]==oldColor){
-            image[newX][newY]=color;
-            q.push({newX,newY});
-           
-          }
-
-        }
-        
-
-      }
-   }
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-
-      int m=image.size();
-      int n=image[0].size();
-     
-      bfs(image,sr,sc,color);
+      dfs(image,sr,sc,color,oldColor);
       return image;
         
     }
