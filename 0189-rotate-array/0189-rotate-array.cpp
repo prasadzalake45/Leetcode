fa70074911df6bbd60e3b92ab0f56e313f5a621c
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2025-07-06 11:29:38 +0530

    Time: 0 ms (100%), Space: 31.9 MB (6.25%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -3,22 +3,31 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n=nums.size();
         k=k%n;
-        vector<int>temp;
 
-
-        for(int i=n-k;i<n;i++){
-            temp.push_back(nums[i]);
+        if(n<k){
+          return;
         }
 
-        for(int i=0;i<n-k;i++){
-            temp.push_back(nums[i]);
+
+        vector<int>arr1;
+        vector<int>arr2;
+        vector<int>answer;
+
+        // lastly k element
+
+        for(int i=n-1;i>=n-k;i--){
+          arr1.push_back(nums[i]);
+        }
+        reverse(arr1.begin(),arr1.end());
+
+        for(int i=0 ;i<n-k;i++){
+          arr2.push_back(nums[i]);
         }
 
-        for(int i=0;i<temp.size();i++){
-            nums[i]=temp[i];
-           
-        }
 
-        
+        answer.insert(answer.end(),arr1.begin(),arr1.end());
+        answer.insert(answer.end(),arr2.begin(),arr2.end());
+
+        nums=answer;
     }
 };
