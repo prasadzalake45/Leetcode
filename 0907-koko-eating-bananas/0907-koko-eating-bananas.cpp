91daec1ea53c4283c0f9cfc36621b10fede2d562
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2025-07-20 12:01:43 +0530

    Time: 13 ms (28.21%), Space: 22.9 MB (42.11%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -1,37 +1,58 @@
 class Solution {
 public:
 
-    long long  helper(vector<int>&piles,int mid){
-        long long sum=0;
-        for(int i=0;i<piles.size();i++){
-            sum+=ceil(double(piles[i])/mid);
-           
-            
-        }
-        return sum;
+    long long helper(vector<int>&piles,int ans){
+
+      long long sum=0;
+
+      for(int i=0;i<piles.size();i++){
+
+        sum+=ceil((double)piles[i]/ans);
+
+
+
+      }
+
+     
+
+      return sum;
+
 
     }
     int minEatingSpeed(vector<int>& piles, int h) {
 
-        int n=piles.size();
+      int n=piles.size();
 
-        int mini=1;
-        int maxi=*max_element(piles.begin(),piles.end());
-        long ans=maxi;
-        while(mini<=maxi){
-            int mid=(mini+maxi)/2;
-            if(helper(piles,mid)<=h){
-                ans=mid;
-                maxi=mid-1;
+      int low=1;
+      int high=*max_element(piles.begin(),piles.end());
 
-            }
-            else{
-                mini=mid+1;
-            }
+
+      // for(int i=mini;i<=maxi;i++){
+      //   if(helper(piles,i)<=h){
+      //     return i;
+      //   }
+      // }
+      // return -1;
+
+      int ans=-1;
+
+      while(low<=high){
+
+        int mid=(low+high)/2;
+
+        if(helper(piles,mid)<=h){
+          ans=mid;
+          high=mid-1;
         }
-        return ans;
+        else{
+          low=mid+1;
+        }
 
+        
 
+      }
+
+      return ans;
         
     }
 };
