dc4bbf42ffe89ec42983008dcb9585f337e17ff0
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2025-09-28 17:27:29 +0530

    Time: 3 ms (32.49%), Space: 69.5 MB (58.97%) - LeetHub

--- 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
@@ -5,27 +5,27 @@ public:
 
         int i = 0;
         int j = 0;
-
         int zeros = 0;
         int maxi = INT_MIN;
+
         while (j < n) {
 
             if (nums[j] == 0) {
                 zeros++;
 
                 while (zeros > k) {
+                   
 
-                    if(nums[i]==0){
+                    if (nums[i] == 0) {
                         zeros--;
                     }
-                    i++;
+                     i++;
                 }
             }
 
             maxi = max(maxi, j - i + 1);
             j++;
         }
-
 
         return maxi;
     }
