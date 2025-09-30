9690ba0fd32bd9574bfed84982d1619d3a72e337
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2025-09-30 11:27:50 +0530

    Time: 13 ms (35.18%), Space: 38.6 MB (24.89%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
@@ -4,34 +4,25 @@ public:
 
         int n=nums.size();
 
-
-        int cnt=0;
-
-        int sum=0;
-
         unordered_map<int,int>mp;
         mp[0]=1;
+        int cnt=0;
+        int sum=0;
+
         for(int i=0;i<n;i++){
 
             sum+=nums[i];
 
-          
-
-
-
-             if(mp.find(sum-goal)!=mp.end()){
-                cnt+=mp[sum-goal];
+            if(mp.find(sum-goal)!=mp.end()){
+                 cnt+=mp[sum-goal];
             }
-               mp[sum]++;
+
+            mp[sum]++;
+
 
         }
 
-
         return cnt;
-
-
-         
-     
         
     }
 };
