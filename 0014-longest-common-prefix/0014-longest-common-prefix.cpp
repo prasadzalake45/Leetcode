3bf07068615030d6e291d463f88e67128ed61428
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2025-10-05 19:19:10 +0530

    Time: 0 ms (100%), Space: 12 MB (50.01%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -3,26 +3,31 @@ public:
     string longestCommonPrefix(vector<string>& strs) {
 
         int n=strs.size();
-
         string temp=strs[0];
 
         for(int i=1;i<n;i++){
+          
+
+           int j=0;
+
+           while(j<strs[i].size() && j<temp.size() && temp[j]==strs[i][j]){
+            j++;
+           }
+
+           temp=temp.substr(0,j);
 
 
-            int j=0;
+           if(temp==""){
+            return "";
+           }
 
-            while(j<temp.size() && j<strs[i].size() && temp[j]==strs[i][j]){
-                j++;
-            }
 
-            temp=temp.substr(0,j);
 
-             if(temp.empty()){
-                return "";
-             }
         }
 
         return temp;
-        
+
+
+
     }
 };
