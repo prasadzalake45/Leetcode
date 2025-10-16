b3a3136cf83280b67febc5b45f7439cab7c54013
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2025-10-17 02:59:13 +0530

    Time: 3 ms (13.89%), Space: 9.1 MB (54.71%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,43 +1,47 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        long long  n=s.size();
 
+        // leading whiteSpace REMOVE  (Done)
+        // leading ZEROS REMOVE
+        // leading -ve sign consider as -VE
+        // leadind with word Consider 0
+        // leading with zeros after not digit consider 0
+        // leading minus only should be consider
 
+        int n = s.size();
+        int i = 0;
+        int sign = 1;
+        long long result = 0;
 
-        int i=0;
-        long long result=0;
-
-        int sign=1;
-
-        while(i<n && s[i]==' '){
+        while (i < n && s[i] == ' ') {
             i++;
         }
 
-        if(i<n && (s[i]=='+' ||s[i]=='-')){
-            sign=(s[i]=='-')?-1:1;
+        if (i < n && (s[i] == '-' || s[i] == '+')) {
+
+            if (s[i] == '+') {
+                i++;
+            } else {
+                sign = -1;
+                i++;
+            }
+        }
+
+        while (i < n && isdigit(s[i])) {
+            result = (result * 10) + (s[i] - '0');
+
+            if (result*sign > INT_MAX) {
+                return INT_MAX;
+            }
+
+            if (result*sign < INT_MIN) {
+                return INT_MIN;
+            }
             i++;
         }
-
-        while(i<n && isdigit(s[i])){
-            result=result*10+(s[i]-'0');
-            i++;
-
-             if(sign*result>INT_MAX){
-            return INT_MAX;
-        }
-        if(sign*result<INT_MIN){
-            return INT_MIN;
-        }
-
-
-        }
-
-       
-
-        return sign* result;
-
-        
-        
+        cout<<sign<<endl;
+        cout<<result<<endl;
+        return (int)(sign * result);
     }
 };
