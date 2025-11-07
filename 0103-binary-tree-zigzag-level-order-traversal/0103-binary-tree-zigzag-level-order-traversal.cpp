c4492c409c11dffd670ccfba301272f3aa4728b8
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2025-11-08 02:06:48 +0530

    Time: 0 ms (100%), Space: 15.3 MB (17.51%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -11,63 +11,49 @@
  */
 class Solution {
 public:
-
-    void helper(TreeNode* root,vector<vector<int>>&answer){
-
-        
-
-        queue<TreeNode*>q;
-        q.push(root);
-
-        bool leftToRight=true;
-
-
-        while(!q.empty()){
-
-            int size=q.size();
-            vector<int>vec(size);
-
-            for(int i=0;i<size;i++){
-                TreeNode* node=q.front();
-                q.pop();
-
-                // if(leftToRight==true){
-                //     vec[i]=node->val;
-
-                // }
-                // else if(leftToRight==false){
-                //     vec[size-i-1]=node->val;
-                // }
-
-                int ind=leftToRight?i:size-i-1;
-
-                vec[ind]=node->val;
-
-                if(node->left){
-                    q.push(node->left);
-                }
-
-                if(node->right){
-                    q.push(node->right);
-                }
-            }
-            leftToRight=!leftToRight;
-            answer.push_back(vec);
-
-        }
-
-
-
-    }
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+
+        // bfs
 
         if(root==NULL){
             return {};
         }
 
-        vector<vector<int>>answer;
-        helper(root,answer);
-        return answer;
+        vector<vector<int>>ans;
+
+        queue<TreeNode*>q;
+        q.push(root);
+        bool zigzag=true;
+
+        while(!q.empty()){
+            int n=q.size();
+            vector<int>res;
+
+            for(int i=0;i<n;i++){
+                TreeNode* cur=q.front();
+                q.pop();
+                res.push_back(cur->val);
+
+                if(cur->left){
+                    q.push(cur->left);
+                }
+                if(cur->right){
+                    q.push(cur->right);
+                }
+            }
+            if(zigzag){
+                ans.push_back(res);
+            }
+            if(!zigzag){
+                reverse(res.begin(),res.end());
+                ans.push_back(res);
+            }
+            zigzag=!zigzag;
+        }
+
+
+        return ans;
+        
         
     }
 };
