9cb0dc14f52c0ee814fedea153ab247c2a7fa042
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2025-11-13 12:01:02 +0530

    Time: 3 ms (29.41%), Space: 16.8 MB (74.13%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,84 +1,99 @@
 class Solution {
 public:
+    void goToFourDirection(vector<vector<int>>&grid,int i,int j,queue<pair<int,int>>&q,bool& pathToMake,int& fresh){
 
-   int bfs(int row,int col,vector<vector<int>>&grid,vector<vector<int>>&visited){
-
-    queue<pair<int,int>>q;
-    q.push({row,col});
-    visited[row][col]=2;
-    int cnt=0;
-    
-
-    while(!q.empty()){
-
-      int size=q.size();
-       bool taken=false;
-
-
-      for(int i=0;i<size;i++){
-
-      
-      auto [x,y]=q.front();
-      q.pop();
-      
-    
-      int dx[]={0,1,0,-1};
-      int dy[]={1,0,-1,0};
-    
-
-      for(int i=0;i<4;i++){
-       
-        int newX=x+dx[i];
-        int newY=y+dy[i];
-
-        if(newX>=0 && newX<grid.size() && newY>=0 && newY<grid[0].size() && grid[newX][newY]==1 && visited[newX][newY]!=2){
-          
-          visited[newX][newY]=2;
-          q.push({newX,newY});
-          taken=true;
-          
+        int m=grid.size();
+        int n=grid[0].size();
+        
+        //left
+        
+        if(j-1>=0 && grid[i][j-1]==1){
+          q.push({i,j-1});
+          grid[i][j-1]=2;  
+          pathToMake=true;
+            fresh--;
         }
-      }
-      
 
-      }
+        //right
+        if(j+1<n && grid[i][j+1]==1){
+            q.push({i,j+1});
+            grid[i][j+1]=2;
+            pathToMake=true;
+              fresh--;
+        }
 
-    if(taken){
-      cnt++;
+        // up
+
+        if(i-1>=0 && grid[i-1][j]==1){
+            q.push({i-1,j});
+            grid[i-1][j]=2;
+            pathToMake=true;
+              fresh--;
+        }
+
+        // down
+
+        if(i+1<m && grid[i+1][j]==1){
+            q.push({i+1,j});
+            grid[i+1][j]=2;
+            pathToMake=true;
+              fresh--;
+        }
+
+
+        
+        
+
+
+
     }
-
-
-
-    }
-    return cnt;
-
-
-   }
     int orangesRotting(vector<vector<int>>& grid) {
-      int m=grid.size();
-      int n=grid[0].size();
-      vector<vector<int>>visited=grid;
-      int ans=0;
+        
+        queue<pair<int,int>>q;
+        int fresh=0;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[i].size();j++){
+                if(grid[i][j]==2){
+                    q.push({i,j});
+                }
+                else if(grid[i][j]==1){
+                    fresh++;
 
-      for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-          if(grid[i][j]==2){
-            ans=bfs(i,j,grid,visited);
-          }
+                }
+            }
         }
-      }
+        int minute=0;
+        
+        while(!q.empty()){
 
-      for(int i=0;i<visited.size();i++){
-        for(int j=0;j<visited[0].size();j++){
-          if(visited[i][j]==1){
-            return -1;
-          }
-          // cout<<visited[i][j]<<" ";
+            int n=q.size();
+            bool pathTomake=false;
+
+            for(int i=0;i<n;i++){
+               int first=q.front().first;
+            int second=q.front().second;
+            q.pop();
+
+            goToFourDirection(grid,first,second,q,pathTomake,fresh);
+            }
+
+            if(pathTomake==true){
+ minute++;
+            }
+
+           
+            
+            
+
+
         }
-        // cout<<endl;
-      }
+        
 
-      return ans;
-       
+
+         return fresh==0?minute:-1;
+
+
+     
+        
     }
 };
