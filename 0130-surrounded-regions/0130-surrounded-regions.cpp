85545097e382e5a57ef5d1b9891d59123ef64304
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2025-11-15 20:03:25 +0530

    Time: 3 ms (25.93%), Space: 14.5 MB (59.57%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,83 +1,70 @@
 class Solution {
 public:
-   void helper(vector<vector<char>>& board,queue<pair<int,int>>&q){
-    
 
-    while(!q.empty()){
-      auto [x,y]=q.front();
-      q.pop();
-
-      board[x][y]='#';
-
-      int dx[]={0,1,0,-1};
-      int dy[]={1,0,-1,0};
-
-      for(int i=0;i<4;i++){
-
-        int newX=x+dx[i];
-        int newY=y+dy[i];
-
-        if(newX>=0 && newX<board.size() && newY>=0 && newY<board[0].size()){
-          if(board[newX][newY]=='O'){
-            q.push({newX,newY});
-            board[newX][newY]='#';
-            
-
-          }
-        }
-
-      }
-    }
-
-   }
-    void solve(vector<vector<char>>& board) {
-
-      int m=board.size();
-      int n=board[0].size();
-      queue<pair<int,int>>q;
-
-      // row first and last
-      for(int j=0;j<n;j++){
-        if(board[0][j]=='O'){
-          q.push({0,j});
-        }
-
-        if(board[m-1][j]=='O'){
-          q.push({m-1,j});
-        }
-      }
-
-
-      //column  first and last
-
-       for(int i=1;i<m-1;i++){
-        if(board[i][0]=='O'){
-          q.push({i,0});
-        }
-
-        if(board[i][n-1]=='O'){
-          q.push({i,n-1});
-        }
-      }
-
-      helper(board,q);
-
-
-    for(int i=0;i<m;i++){
-      for(int j=0;j<n;j++){
-        if(board[i][j]=='#'){
-          board[i][j]='O';
-        }
-        else{
-          board[i][j]='X';
-
-        }
-      }
-    }
-
-
+   void dfs(int i,int j,vector<vector<char>>&board,vector<vector<int>>&visited){
 
      
+    visited[i][j]=1;
+
+    int m = board.size();
+    int n = board[0].size();
+    
+     
+ 
+      
+     // left
+     if(j-1>=0 && board[i][j-1]=='O' && !visited[i][j-1]){
+ 
+         dfs(i,j-1,board,visited);
+      }
+
+      // right
+      
+      if(j+1<n && board[i][j+1]=='O' && !visited[i][j+1]){
+        
+         dfs(i,j+1,board,visited);
+
+      }
+
+      //up 
+
+      
+      if(i-1>=0 && board[i-1][j]=='O' && !visited[i-1][j]){
+        
+         dfs(i-1,j,board,visited);
+        
+      }
+
+      // down
+      
+      if(i+1<m && board[i+1][j]=='O' && !visited[i+1][j]){
+     
+        dfs(i+1,j,board,visited);
+      }
+   }
+    void solve(vector<vector<char>>& board) {
+        int m=board.size();
+        int n=board[0].size();
+
+       vector<vector<int>>visited(m,vector<int>(n,0));
+
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if((i==0 || j==0 || i==m-1 || j==n-1)&& board[i][j]=='O'){
+                    dfs(i,j,board,visited);
+                }
+            }
+        }
+
+
+       for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(visited[i][j]==0){
+                board[i][j]='X';
+            }
+        }
+       }
         
     }
 };
