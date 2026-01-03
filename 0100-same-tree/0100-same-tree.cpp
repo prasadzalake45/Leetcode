7b8893d78807ff00269276295f66b76e0451766e
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2026-01-03 20:50:11 +0530

    Time: 0 ms (100%), Space: 12.8 MB (76.79%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -6,33 +6,49 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    bool helper(TreeNode* p, TreeNode* q) {
+
+    bool helper(TreeNode* p,TreeNode* q){
+        if(p==NULL && q==NULL){
+            return true;
+        }
+
+        if(p==NULL || q==NULL){
+            return false;
+        }
+
+        if(p->val!=q->val){
+            return false;
+        }
+
+
+
+        bool lh=helper(p->left,q->left);
+        bool rh=helper(p->right,q->right);
+
+        if(lh==true && rh==true){
+            return true;
+        }
+        return false;
+    }
+    bool isSameTree(TreeNode* p, TreeNode* q) {
 
         if(p==NULL && q==NULL){
             return true;
         }
 
-        if (p == NULL || q == NULL){
+        if(p==NULL || q==NULL){
             return false;
         }
 
-        if (p->val != q->val) {
-            return false;
-        }
+        
 
-        bool lh = helper(p->left, q->left);
-        bool rh = helper(p->right, q->right);
-        return lh && rh;
-    }
-    bool isSameTree(TreeNode* p, TreeNode* q) 
-    {
-         bool ans= helper(p, q); 
-         return ans;
+        return helper(p,q);
+
+        
     }
 };
