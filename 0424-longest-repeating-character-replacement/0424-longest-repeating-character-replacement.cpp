3bb61d09f368ec089a6ffb6b759e9c91d1046296
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2026-01-10 19:28:38 +0530

    Time: 11 ms (29.81%), Space: 11.2 MB (5.27%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,33 +1,61 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
+         
+    int n=s.size();
+    
+    int i=0;
+    int j=0;
+    int maxi=INT_MIN;
+    int cnt=0;
 
-        int n=s.size();
+    unordered_map<char,int>mp;
+    while(j<n){
 
-        int i=0;
-        int j=0;
-        int cnt=0;
-        int maxi=0;
-        unordered_map<char,int>mp;
+        mp[s[j]]++;
 
-        while(j<n){
+        cnt=max(cnt,mp[s[j]]);
 
-            mp[s[j]]++;
 
-            cnt=max(cnt,mp[s[j]]);
+        int replacementOption=(j-i+1)-cnt;
 
-            if((j-i+1)-cnt>k){
-                mp[s[i]]--;
+        if(replacementOption>k){
+              mp[s[i]]--;
                 if(mp[s[i]]==0){
                     mp.erase(s[i]);
                 }
                 i++;
-            }
-
-            maxi=max(maxi,j-i+1);
-            j++;
 
         }
-        return maxi;
+
+        maxi=max(maxi,j-i+1);
+
+        j++;
+
+
+
+
+        
+        
+  
+
+           
+      
+
+
+
+
+    }
+
+    return maxi;
+
+   
+
+       
+
+
+
+
+        
     }
 };
