97e9d0518ae5686d801b2ba8eb49a8edf762e35b
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2026-01-15 02:42:51 +0530

    Time: 11 ms (23.53%), Space: 21.2 MB (9.9%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,51 +1,65 @@
 class Solution {
 public:
- bool dfs(int node,vector<vector<int>>&adjMatrix,vector<int>&visited,vector<int>&pathVisited){
-      visited[node]=1;
-      pathVisited[node]=1;
-      
-      for(auto neigh:adjMatrix[node]){
-          if(!visited[neigh]){
-             if(dfs(neigh,adjMatrix,visited,pathVisited)==true){
-                 return true;
-             }
-        }
-        else if(pathVisited[neigh]){
-            return true;
-        }
-      }
-
-
-      pathVisited[node]=0;
-      
-      
-      return false;
-  }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-    
-    
-    vector<vector<int>>adjMatrix(numCourses);
-        
-        
-        
-        for(int i=0;i<prerequisites.size();i++){
-            int u=prerequisites[i][0];
-            int v=prerequisites[i][1];
-            adjMatrix[u].push_back(v);
-        }
-        
-        vector<int>visited(numCourses,0);
-        vector<int>pathVisited(numCourses,0);
 
+        vector<vector<int>> adjList(numCourses);
+
+        for ( auto it : prerequisites) {
+
+            int u = it[0];
+            int v = it[1];
+            adjList[u].push_back(v);
+        }
+
+        vector<int> indegree(numCourses, 0);
+
+        for (auto it : prerequisites) {
+            int u = it[0];
+            int v = it[1];
         
-        for(int i=0;i<numCourses;i++){
-            if((!visited[i])){
-                if(dfs(i,adjMatrix,visited,pathVisited)==true){
-                    return false;
-                }
+            indegree[v] += 1;
+        }
+
+        queue<int> q;
+
+        for (int i = 0; i < indegree.size(); i++) {
+            if (indegree[i] == 0) {
+                q.push(i);
             }
         }
-        return true;
 
+        if (q.empty()) {
+            return false;
+        }
+
+        stack<int>st;
+
+
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            st.push(node);
+            
+
+            for (auto it:adjList[node]){
+
+                
+                    indegree[it]--;
+
+                    if(indegree[it]==0){
+                        q.push(it);
+                    }
+                
+
+            }
+        }
+
+
+        if(st.size()==numCourses){
+            return true;
+        }
+
+
+        return false;
     }
 };
