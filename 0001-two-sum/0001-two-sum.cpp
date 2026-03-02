751460ed04b887af431a62e5d8352c2fc9cc5be4
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2026-03-02 13:26:25 +0530

    Time: 2 ms (73.02%), Space: 14.8 MB (52.13%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,25 +1,35 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
 
+    // each i/p has excatlty one solutions
+    // not to used same element twice
+    //imp
+    // 1) consider does it affect when there is array is sorted or not
+    vector<int> twoSum(vector<int>& nums, int target) {
         int n=nums.size();
         unordered_map<int,int>mp;
 
 
-        for(int i=0;i<n;i++){
+        
 
+        for(int i=0;i<n;i++){
+ 
             int ans=target-nums[i];
+
 
             if(mp.find(ans)!=mp.end()){
                 return {mp[ans],i};
-                break;
             }
             else{
                 mp[nums[i]]=i;
-            }
 
+            }
         }
 
+        // if(mp.find(sum-target)!=mp.end()){
+        //     return {mp[sum-target],n-1};
+        //  }
+        
         return {};
         
     }
