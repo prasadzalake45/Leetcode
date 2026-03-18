183d2a2e8b4c477eebb86105d904ca69d0551624
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2026-03-18 12:19:10 +0530

    Time: 3 ms (30.43%), Space: 10.2 MB (23.8%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,47 +1,27 @@
 class Solution {
 public:
-    bool checkAlpha(char ch){
-        cout<<"IN LOOP "<<ch<<endl;
-        for(char i='a';i<'z';i++){
-            if(i==ch){
-                return true;
-            }
-        }
-
-        for(char i='0';i<='9';i++){
-            if(i==ch){
-                return true;
-            }
-        }
-        
-       
-        return false;
-    }
+   
     bool isPalindrome(string s) {
-        //remove non -alphanumeric
-        //uppercase-> lowercase
-        //make sure bring it as one string and check if it Palindrone
- 
+        // remove non -alphanumeric
+        // uppercase-> lowercase
+        // make sure bring it as one string and check if it Palindrone
 
-        string str="";
-        for(int i=0;i<s.size();i++){
-        
-            char ch=tolower(s[i]);
-            cout<<"lower"<<ch<<endl;
-            if(checkAlpha(ch)){
-                cout<<"IN"<<ch<<endl;
+        string str = "";
+        for (int i = 0; i < s.size(); i++) {
+            char ch = tolower(s[i]);
+            if (isalnum(ch)) {
                 str.push_back(ch);
             }
         }
 
-        cout<<str<<endl;
-        int n=str.size();
-        
-        int left=0;
-        int right=n-1;
+        cout << str << endl;
+        int n = str.size();
 
-        while(left<right){
-            if(str[left]!=str[right]){
+        int left = 0;
+        int right = n - 1;
+
+        while (left < right) {
+            if (str[left] != str[right]) {
                 return false;
             }
             left++;
@@ -49,6 +29,5 @@ public:
         }
 
         return true;
-
     }
 };
