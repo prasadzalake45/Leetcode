22f4cecec1bc6797ebc20dc7674e46ecf249e34f
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2026-03-18 12:10:17 +0530

    Time: 1067 ms (6.23%), Space: 10.3 MB (23.8%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,45 +1,54 @@
 class Solution {
 public:
-    bool isPalindrome(string s) {
-        
-
-        
-
-        // all convert into upper->lower
-        // remove all non -alpha 
-        //now check that string valindrone or not
-        string str="";
-
-
-        for(char ch:s){
-            if(isalnum(ch)){
-                str+=tolower(ch);
+    bool checkAlpha(char ch){
+        cout<<"IN LOOP "<<ch<<endl;
+        for(char i='a';i<'z';i++){
+            if(i==ch){
+                return true;
             }
         }
+
+        for(char i='0';i<='9';i++){
+            if(i==ch){
+                return true;
+            }
+        }
+        
+       
+        return false;
+    }
+    bool isPalindrome(string s) {
+        //remove non -alphanumeric
+        //uppercase-> lowercase
+        //make sure bring it as one string and check if it Palindrone
+ 
+
+        string str="";
+        for(int i=0;i<s.size();i++){
+        
+            char ch=tolower(s[i]);
+            cout<<"lower"<<ch<<endl;
+            if(checkAlpha(ch)){
+                cout<<"IN"<<ch<<endl;
+                str.push_back(ch);
+            }
+        }
+
         cout<<str<<endl;
         int n=str.size();
-
-        if(n==0){
-            return true;
-        }
-    
         
         int left=0;
         int right=n-1;
-
 
         while(left<right){
             if(str[left]!=str[right]){
                 return false;
             }
-            else{
-                left++;
-                right--;
-            }
+            left++;
+            right--;
         }
 
         return true;
-        
 
     }
 };
