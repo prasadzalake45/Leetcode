0adf657be32ae5b8d9baf55a174880df7a3b0858
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2026-03-28 19:31:25 +0530

    Time: 343 ms (13.91%), Space: 291.7 MB (8.88%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
 
-    bool checkPalindrone(string& s,int start,int end){
+    bool checkPalindrone(string& s,int start,int end,vector<vector<int>>& dp){
         if(start>=end){
-            return true;
+            return 1;
+        }
+
+        if(dp[start][end]!=-1){
+            return dp[start][end];
         }
 
         if(s[start]==s[end]){
-            return checkPalindrone(s,start+1,end-1);
+            return dp[start][end]=checkPalindrone(s,start+1,end-1,dp);
         }
 
-        return false;
+        return dp[start][end]=0;
 
         
 
@@ -21,9 +25,11 @@ public:
         int maxi=1;
         int startIdx=0;
 
+
+vector<vector<int>> dp(n, vector<int>(n, -1));
         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
-                if(checkPalindrone(s,i,j)==true){
+                if(checkPalindrone(s,i,j,dp)==true){
                     if(j-i+1>maxi){
                        maxi=j-i+1;
                        startIdx=i;
