b554abd692cb0b1def5b4c0b975be39d328896e8
Prasad Zalake <109515192+prasadzalake45@users.noreply.github.com>
2026-05-20 11:39:47 +0530

    Time: 3 ms (73.5%), Space: 85.7 MB (85.47%) - LeetHub

--- 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.cpp
+++ 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.cpp
@@ -1,52 +1,33 @@
 class Solution {
 public:
-    vector<int>helper(vector<int>& A, vector<int>& B,vector<int>&C){
-        unordered_map<int,int>mp1;
-        unordered_map<int,int>mp2;
-
-        int i=0;
-        int j=0;
-
-        int n1=A.size();
-        int n2=B.size();
-
-        while(i<n1 && j<n2){
-            if(i!=0 && j!=0){
-                C[i]=C[i-1];
-            }
-
-            if(A[i]==B[j]){
-              C[i]+=1;
-            }
-            if(mp1.find(B[j])!=mp1.end()){
-                C[i]+=1;
-            }
-            if(mp2.find(A[i])!=mp2.end()){
-                C[i]+=1;
-            }
-
-            mp1[A[i]]++;
-            mp2[B[j]]++;
-           
-            i++;
-            j++;
-        }
-
-        return C;
-    }
     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
-        
-
-        // find the prefix common array till that index from two array
-         
-        //approach
-        //1) first check if it equal yes increase the cnt
-        //2) check on map it present 
-
 
         int n=A.size();
         vector<int>C(n,0);
-        return helper(A,B,C);
 
+        vector<int>freq(n+1,0);
+
+        int common=0;
+
+
+        for(int i=0;i<n;i++){
+
+
+              freq[A[i]]++;
+
+              if(freq[A[i]]==2){
+                common++;
+              }
+
+              freq[B[i]]++;
+
+              if(freq[B[i]]==2){
+                common++;
+              }
+
+            C[i]=common;
+        }
+
+        return C;
     }
 };
